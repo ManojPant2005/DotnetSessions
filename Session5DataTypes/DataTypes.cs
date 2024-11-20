29a12c3b51d1e0a53fefaa6deb0720175603af6d
Manoj Pant <pantmanoj2005@gmail.com>
2024-11-20 22:14:33 +0530

    Add project files.

---
+++ Session5DataTypes/DataTypes.cs
@@ -0,0 +1,56 @@
+﻿// C# Data Types :
+// string | int | long | double | char | bool  | float
+
+
+//String | alphabetic character
+var productName = "This is new variable";
+var Description = "This is demo description";
+var Message = "This is new message";
+var MyAge = "12";
+
+string MyVariable = "This is new string";
+string UpdatedName = "UpdatedName";
+
+string UserName = "Username";
+string Password = "Password";
+
+
+//Int | contains numeric values
+
+int newAge = 12;
+int MobileNumber = 2131241223;
+int MyAadharNumber = 34634346;
+int studentId = 23;
+var myKeyNumber = 2523532;
+var OTP = 24525;
+
+//Long : stores long int numbers
+long myAadhar = 1234567890123;
+var myAccount = 1234567890987;
+
+//double : stores pointer values
+
+var myMarksGrade = 4.89;
+double priceTotal = 23.456;
+double myCharacters = 20.34534534234234;
+
+//Char : Contains only one character 
+
+char myGrade =  'A';
+var MyGender = 'M';
+
+
+//Float : limitation of storing pointer values
+
+float myNum = 5.75F;
+float myChar = 6.23F;
+
+//Bool : contains only true or false values
+
+bool IsAdmin = true;
+bool IsGuest = true;
+bool IsOnline = false;
+var IsBusy = true;
+
+
+Console.WriteLine(MobileNumber);
---
+++ Session5DataTypes/Variables.cs
@@ -0,0 +1,43 @@
+﻿
+//printing statement
+
+// This is single line comment
+
+/*
+
+  This is multiline comment 
+ 
+*/
+
+//Console.WriteLine("Coding Clown!");
+
+var myName = "My Name";
+var myAge = 22;
+
+
+myName = "Coding Clown..!";
+myAge = 34;
+
+
+
+
+
+Console.WriteLine(myName);
+Console.WriteLine(myAge);
+
+//Naming convention
+//Camel case | Snake case
+
+var updatedName = "This is camel case naming convention";
+
+var _newName = "This is snake case";
+Console.WriteLine(updatedName);
+Console.WriteLine(_newName);
+
+
+
+
+
+
+
+
